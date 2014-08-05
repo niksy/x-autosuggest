19a53087dd8c06a012460e3e6f347eb2a63c74dd
Ivan Nikolić <niksy5@gmail.com>
2014-08-05 15:53:02 +0200

    CSS file

--- Gruntfile.js
+++ Gruntfile.js
@@ -90,7 +90,7 @@ module.exports = function ( grunt ) {
 	grunt.loadNpmTasks('grunt-bump');
 
 	grunt.registerTask('stylecheck', ['jshint:main', 'jscs:main']);
-	grunt.registerTask('default', ['stylecheck', 'concat', 'uglify']);
+	grunt.registerTask('default', ['stylecheck', 'concat', 'uglify', 'cssmin']);
 	grunt.registerTask('releasePatch', ['bump-only:patch', 'default', 'bump-commit']);
 	grunt.registerTask('releaseMinor', ['bump-only:minor', 'default', 'bump-commit']);
 	grunt.registerTask('releaseMajor', ['bump-only:major', 'default', 'bump-commit']);
