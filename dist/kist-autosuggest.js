19663de04b5849221a1541e59b77555ba7b99f6e
Ivan Nikolić <ivan.nikolic@trikoder.net>
2015-01-30 15:12:55 +0100

    Release 0.2.1

--- dist/kist-autosuggest.js
+++ dist/kist-autosuggest.js
@@ -1,4 +1,4 @@
-/*! kist-autosuggest 0.2.0 - Simple autosuggest plugin. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2015 | License: MIT */
+/*! kist-autosuggest 0.2.1 - Simple autosuggest plugin. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2015 | License: MIT */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self);var n=f;n=n.jQuery||(n.jQuery={}),n=n.fn||(n.fn={}),n.autosuggest=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 // jscs:disable requireCapitalizedComments
@@ -650,8 +650,11 @@ function getClassSelector ( className ) {
 function inputEventHandler ( cb, e ) {
 
 	var keycode = e.which;
+	var val = $(e.target).val();
 
-	this.setInputVal($(e.target).val(), true);
+	emit(this, 'input', [val]);
+
+	this.setInputVal(val, true);
 
 	// If it’s not standard alphanumeric key
 	// or length of current string is to small
