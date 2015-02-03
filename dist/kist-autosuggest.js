7bc44ac965abd583710bc3b22ab85e81290ee74f
Ivan Nikolić <ivan.nikolic@trikoder.net>
2015-02-03 11:46:20 +0100

    Release 0.2.2

--- dist/kist-autosuggest.js
+++ dist/kist-autosuggest.js
@@ -1,4 +1,4 @@
-/*! kist-autosuggest 0.2.1 - Simple autosuggest plugin. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2015 | License: MIT */
+/*! kist-autosuggest 0.2.2 - Simple autosuggest plugin. | Author: Ivan Nikolić <niksy5@gmail.com> (http://ivannikolic.com/), 2015 | License: MIT */
 !function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self);var n=f;n=n.jQuery||(n.jQuery={}),n=n.fn||(n.fn={}),n.autosuggest=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 (function (global){
 // jscs:disable requireCapitalizedComments
@@ -650,9 +650,7 @@ function getClassSelector ( className ) {
 function inputEventHandler ( cb, e ) {
 
 	var keycode = e.which;
-	var val = $(e.target).val();
-
-	emit(this, 'input', [val]);
+	var val = this.$el.val();
 
 	this.setInputVal(val, true);
 
@@ -756,6 +754,13 @@ module.exports = {
 
 		this.$el
 			.on('keyup' + this.ens, debounce(300, $.proxy(inputEventHandler, this, this.getData)))
+			.on('keyup' + this.ens, $.proxy(function ( e ) {
+
+				if ( $.inArray(e.which, blacklist) === -1 ) {
+					emit(this, 'input', [this.$el.val()]);
+				}
+
+			}, this))
 			.on('keydown' + this.ens, $.proxy(keyboardNavigation, this))
 			.on('focus' + this.ens, $.proxy(this.showResults, this))
 			.on('focus' + this.ens, $.proxy(function ( e ) {
